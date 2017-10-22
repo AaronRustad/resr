eae4dfe62df2362a7c3f16fea216dc2806258f51
Aaron Rustad <arustad@anassina.com>
2017-10-22 07:12:18 -0600

    Set default resr.yml location and allow RESR_CONFIG environment variable to define custom locations.

--- lib/resr.rb
+++ lib/resr.rb
@@ -4,6 +4,8 @@ require 'slack'
 require 'yaml'
 
 module Resr
+  DEFAULT_CONFIG = '.resr.yml'.freeze
+
   def self.list
     channels
   end
@@ -20,7 +22,33 @@ module Resr
     channel.save
   end
 
+  def self.config
+    @config ||= YAML::load_file(config_file_location)
+  end
+
   private
+
+  def self.config_file_location
+    if ENV['RESR_CONFIG']
+      expanded_file_location = File.absolute_path(ENV['RESR_CONFIG'])
+
+      if File.file?(expanded_file_location)
+        return expanded_file_location
+      else
+        raise "Resr config not found at #{expanded_file_location}"
+      end
+    end
+
+    config_file =
+      case
+      when File.file?(DEFAULT_CONFIG)
+        DEFAULT_CONFIG
+      when File.file?(File.join(Dir.home, DEFAULT_CONFIG))
+        File.join(Dir.home, DEFAULT_CONFIG)
+      else
+        raise 'Resr config has not been found.'
+      end
+  end
 
   def self.channels
     @channels ||= config.dig('channels').map { |name, mappings| State.new(name, mappings, slack_client) }
@@ -30,9 +58,6 @@ module Resr
     config.fetch('slack_token')
   end
 
-  def self.config
-    @config ||= YAML::load_file(File.join(Dir.home, '.resr.yml'))
-  end
 
   def self.slack_client
     @slack_client ||=
--- spec/lib/resr_spec.rb
+++ spec/lib/resr_spec.rb
@@ -5,6 +5,25 @@ RSpec.describe Resr do
     expect(Resr::VERSION).not_to be nil
   end
 
+  describe '.config' do
+    context 'when environment variable is provided' do
+      after { ENV.delete('RESR_CONFIG') }
+
+      context 'with an unknown file' do
+        it 'raises an exception' do
+          ENV['RESR_CONFIG'] = '/tmp/not_found_file.yml'
+          expect { Resr.config }.to raise_error(StandardError, 'Resr config not found at /tmp/not_found_file.yml')
+        end
+      end
+
+      context 'with an known file' do
+        it 'raises an exception' do
+          ENV['RESR_CONFIG'] = './config/resr.yml'
+          expect(Resr.config).to eq({"slack_token"=>"", "channels"=>{"dev-deploy-io"=>{"io"=>":flag-io:", "cat"=>":cat2:", "dog"=>":trashdog:"}, "dev-deploys"=>{"prod"=>":cat2:", "canary"=>":canary:"}}})
+        end
+      end
+    end
+  end
   #describe '.list' do
     #subject { Resr.list }
 
